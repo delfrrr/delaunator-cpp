6cfe88e62001e99f755c7d26c3777deb4ba92d63
Vova Bilonenko <bilonenko.v@gmail.com>
2018-08-28 08:04:59 +0200

    save out to file

--- src/main.cpp
+++ src/main.cpp
@@ -47,62 +47,85 @@ namespace {
         // Points points = {.x_vector = x_vector, .y_vector = y_vector};
         return coords;
     }
+
+    const char* serialize_to_json(Delaunator &delaunator) {
+        rapidjson::StringBuffer sb;
+        rapidjson::PrettyWriter<rapidjson::StringBuffer> writer(sb);
+        writer.StartObject();
+            writer.String("type"); writer.String("FeatureCollection");
+            writer.String("crs");
+                writer.StartObject();
+                    writer.String("type"); writer.String("name");
+                    writer.String("properties");
+                        writer.StartObject();
+                            writer.String("name"); writer.String("urn:ogc:def:crs:EPSG::900913");
+                        writer.EndObject();
+                writer.EndObject();
+            writer.String("features");
+                writer.StartArray();
+                    for(long int i = 0; i < delaunator.triangles.size(); i+=3) {
+                        writer.StartObject();
+                            writer.String("type"); writer.String("Feature");
+                            writer.String("properties"); writer.StartObject(); writer.EndObject();
+                            writer.String("geometry");
+                                writer.StartObject();
+                                    writer.String("type"); writer.String("Polygon");
+                                    writer.String("coordinates");
+                                        writer.StartArray();
+                                        writer.StartArray();
+                                            writer.StartArray();
+                                                writer.Uint(delaunator.coords[2 * delaunator.triangles[i]]);
+                                                writer.Uint(delaunator.coords[2 * delaunator.triangles[i] + 1]);
+                                            writer.EndArray();
+                                            writer.StartArray();
+                                                writer.Uint(delaunator.coords[2 * delaunator.triangles[i + 1]]);
+                                                writer.Uint(delaunator.coords[2 * delaunator.triangles[i + 1] + 1]);
+                                            writer.EndArray();
+                                            writer.StartArray();
+                                                writer.Uint(delaunator.coords[2 * delaunator.triangles[i + 2]]);
+                                                writer.Uint(delaunator.coords[2 * delaunator.triangles[i + 2] + 1]);
+                                            writer.EndArray();
+                                            writer.StartArray();
+                                                writer.Uint(delaunator.coords[2 * delaunator.triangles[i]]);
+                                                writer.Uint(delaunator.coords[2 * delaunator.triangles[i] + 1]);
+                                            writer.EndArray();
+                                        writer.EndArray();
+                                        writer.EndArray();
+                                writer.EndObject();
+                        writer.EndObject();
+                    }
+                writer.EndArray();
+        writer.EndObject();
+        return sb.GetString();
+    }
 }
 
 
 int main(int, char* argv[]) {
     const char* filename = argv[1];
+    const char* output = argv[2];
     string json = read_file(filename);
     const vector<double> coords = get_geo_json_points(json);
     Delaunator delaunator(coords);
-    rapidjson::StringBuffer sb;
-    rapidjson::PrettyWriter<rapidjson::StringBuffer> writer(sb);
-    writer.StartObject();
-        writer.String("type"); writer.String("FeatureCollection");
-        writer.String("crs");
-            writer.StartObject();
-                writer.String("type"); writer.String("name");
-                writer.String("properties");
-                    writer.StartObject();
-                        writer.String("name"); writer.String("urn:ogc:def:crs:EPSG::900913");
-                    writer.EndObject();
-            writer.EndObject();
-        writer.String("features");
-            writer.StartArray();
-                for(long int i = 0; i < delaunator.triangles.size(); i+=3) {
-                    writer.StartObject();
-                        writer.String("type"); writer.String("Feature");
-                        writer.String("properties"); writer.StartObject(); writer.EndObject();
-                        writer.String("geometry");
-                            writer.StartObject();
-                                writer.String("type"); writer.String("Polygon");
-                                writer.String("coordinates");
-                                    writer.StartArray();
-                                    writer.StartArray();
-                                        writer.StartArray();
-                                            writer.Uint(delaunator.coords[2 * delaunator.triangles[i]]);
-                                            writer.Uint(delaunator.coords[2 * delaunator.triangles[i] + 1]);
-                                        writer.EndArray();
-                                        writer.StartArray();
-                                            writer.Uint(delaunator.coords[2 * delaunator.triangles[i + 1]]);
-                                            writer.Uint(delaunator.coords[2 * delaunator.triangles[i + 1] + 1]);
-                                        writer.EndArray();
-                                        writer.StartArray();
-                                            writer.Uint(delaunator.coords[2 * delaunator.triangles[i + 2]]);
-                                            writer.Uint(delaunator.coords[2 * delaunator.triangles[i + 2] + 1]);
-                                        writer.EndArray();
-                                        writer.StartArray();
-                                            writer.Uint(delaunator.coords[2 * delaunator.triangles[i]]);
-                                            writer.Uint(delaunator.coords[2 * delaunator.triangles[i] + 1]);
-                                        writer.EndArray();
-                                    writer.EndArray();
-                                    writer.EndArray();
-                            writer.EndObject();
-                    writer.EndObject();
-                }
-            writer.EndArray();
-    writer.EndObject();
-    puts(sb.GetString());
+    const char* out_json = serialize_to_json(delaunator);
+
+    if (output) {
+        printf("Writing to file %s", output);
+        ofstream stream;
+        stream.open(output);
+        stream << out_json;
+        stream.close();
+        // cout << output << endl;
+    } else {
+        puts(out_json);
+    }
+
+    // cout << output << endl;
+    // if (sizeof(argv) > 2) {
+    //     puts("ouput to file");
+    // } else {
+    //     puts(out_json);
+    // }
     return 0;
     // cout << delaunator.triangles << endl;
 }
