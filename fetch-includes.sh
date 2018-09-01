d8df8f0d4945b37505f953c6034f204add62443f
Vova Bilonenko <bilonenko.v@gmail.com>
2018-09-01 07:22:25 +0200

    use https for includes instead of ssh

--- fetch-includes.sh
+++ fetch-includes.sh
@@ -3,6 +3,6 @@
 mkdir -p ./includes
 rm -rf ./includes/*
 
-git clone --branch v1.1.0 --depth=1 git@github.com:Tencent/rapidjson.git ./includes/rapidjson
-git clone --branch master --depth=1 git@github.com:louisdx/cxx-prettyprint.git ./includes/prettyprint
-git clone --branch master --depth=1 git@github.com:catchorg/Catch2.git ./includes/catch
+git clone --branch v1.1.0 --depth=1 https://github.com/Tencent/rapidjson.git ./includes/rapidjson
+git clone --branch master --depth=1 https://github.com/louisdx/cxx-prettyprint.git ./includes/prettyprint
+git clone --branch master --depth=1 https://github.com/catchorg/Catch2.git ./includes/catch
