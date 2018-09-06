fb0ec55be7a1b1d919f7f1a1ad89e1bd6fd53991
Blake Thompson <flippmoke@gmail.com>
2018-09-06 14:38:06 -0500

    Remove fetch includes as it is no longer required

--- fetch-includes.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir -p ./includes
-rm -rf ./includes/*
-
-git clone --branch v1.1.0 --depth=1 https://github.com/Tencent/rapidjson.git ./includes/rapidjson
-git clone --branch master --depth=1 https://github.com/louisdx/cxx-prettyprint.git ./includes/prettyprint
-git clone --branch master --depth=1 https://github.com/catchorg/Catch2.git ./includes/catch
